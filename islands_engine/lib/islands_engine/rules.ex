19ab23c8fd7fd659047b4f7e9e34e51a107fb86e
Eric Olson <eric.rob.olson@gmail.com>
2021-04-12 06:33:53 -0700

    Implemented first state of FSM.

--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -1,7 +1,9 @@
 defmodule IslandsEngine.Rules do
   alias __MODULE__
 
-  defstruct state: :initialized
+  defstruct state: :initialized,
+            player1: :islands_not_set,
+            player2: :islands_not_set
 
   def new() do
     %Rules{}
@@ -11,7 +13,27 @@ defmodule IslandsEngine.Rules do
     {:ok, %Rules{rules | state: :players_set}}
   end
 
+  def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
+    case Map.fetch!(rules, player) do
+      :islands_set -> :error
+      :islands_not_set -> {:ok, rules}
+    end
+  end
+
+  def check(%Rules{state: :players_set} = rules, {:set_islands, player}) do
+    rules = Map.put(rules, player, :islands_set)
+
+    case both_players_islands_set?(rules) do
+      true -> {:ok, %Rules{rules | state: :player1_turn}}
+      false -> {:ok, rules}
+    end
+  end
+
   def check(_state, _action) do
     :error
+  end
+
+  defp both_players_islands_set?(rules) do
+    rules.player1 == :islands_set && rules.player2 == :islands_set
   end
 end
