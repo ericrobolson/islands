179ad454d0151d7d2171221f6a90bb8d470510ec
Eric Olson <eric.rob.olson@gmail.com>
2021-04-16 06:38:48 -0700

    Added additional genserver behavior.

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -1,8 +1,23 @@
 defmodule IslandsEngine.Game do
+  alias IslandsEngine.{Board, Guesses, Rules}
   use GenServer
+
+  def start_link(name) when is_binary(name) do
+    GenServer.start_link(__MODULE__, name, [])
+  end
+
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+  end
 
   def demo_call(game) do
     GenServer.call(game, :demo_call)
+  end
+
+  def demo_cast(pid, new_value) do
+    GenServer.cast(pid, {:demo_cast, new_value})
   end
 
   def handle_info(:first, state) do
@@ -12,5 +27,9 @@ defmodule IslandsEngine.Game do
 
   def handle_call(:demo_call, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_cast({:demo_cast, new_value}, state) do
+    {:noreply, Map.put(state, :test, new_value)}
   end
 end
