cd2905ebbb10acc77cc12b42e9cd09ba9c8cef83
Eric Olson <eric.rob.olson@gmail.com>
2021-04-22 06:15:51 -0700

    Fixed bug with rules.

--- islands_engine/lib/islands_engine/rules.ex
+++ islands_engine/lib/islands_engine/rules.ex
@@ -44,7 +44,7 @@ defmodule IslandsEngine.Rules do
     end
   end
 
-  def check(%Rules{state: :player2_turn} = rules, {:wincheck, win_or_not}) do
+  def check(%Rules{state: :player2_turn} = rules, {:win_check, win_or_not}) do
     case win_or_not do
       :no_win -> {:ok, rules}
       :win -> {:ok, %Rules{rules | state: :game_over}}
